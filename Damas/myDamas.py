94c2c08ace91ed86d6f5a270a2d7a2dbe069ce44
Mateus Eduardo <mateuseduardosilvar@gmail.com>
2023-06-11 01:29:22 -0300

    Add Reiniciar & Cond Movimentações

--- Damas/myDamas.py
+++ Damas/myDamas.py
@@ -28,24 +28,6 @@ pygame.display.set_caption("Jogo de Damas")
 numeros_linhas = ['1', '2', '3', '4', '5', '6', '7', '8']
 letras_colunas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
-# Posições Iniciais das Peças de damas
-pos_iniciais_brancas = [
-    (0, 1), (0, 3), (0, 5), (0, 7),
-    (1, 0), (1, 2), (1, 4), (1, 6),
-    (2, 1), (2, 3), (2, 5), (2, 7),
-]
-
-pos_iniciais_pretas = [
-    (5, 0), (5, 2), (5, 4), (5, 6),
-    (6, 1), (6, 3), (6, 5), (6, 7),
-    (7, 0), (7, 2), (7, 4), (7, 6),
-]
-
-# Variáveis de controle do jogo
-selecionado = False
-pos_selecionada = None
-vez_das_brancas = True
-
 # Função para obter as casas disponíveis para movimento
 def obter_casas_disponiveis(linha, coluna):
     casas_disponiveis = []
@@ -57,7 +39,27 @@ def obter_casas_disponiveis(linha, coluna):
         casas_disponiveis.append((linha - 1, coluna - 1))
     return casas_disponiveis
 
+# Estado Inicial da partida
+def obter_estado_inicial():
+    global pos_iniciais_brancas, pos_iniciais_pretas, selecionado, pos_selecionada, vez_das_brancas
+    pos_iniciais_brancas = [
+        (0, 1), (0, 3), (0, 5), (0, 7),
+        (1, 0), (1, 2), (1, 4), (1, 6),
+        (2, 1), (2, 3), (2, 5), (2, 7),
+    ]
+
+    pos_iniciais_pretas = [
+        (5, 0), (5, 2), (5, 4), (5, 6),
+        (6, 1), (6, 3), (6, 5), (6, 7),
+        (7, 0), (7, 2), (7, 4), (7, 6),
+    ]
+    
+    selecionado = False
+    pos_selecionada = None
+    vez_das_brancas = True
+
 # Loop principal do jogo e estado do jogo
+obter_estado_inicial()
 no_menu = True
 executando = True
 while executando:
@@ -66,6 +68,20 @@ while executando:
             executando = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             no_menu = True
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+            fonte = pygame.font.Font(None, 80)
+
+            texto_reinit = fonte.render("REINICIANDO...", True, LARANJA)
+            ret_reinit = texto_reinit.get_rect(center=(largura_janela // 2, altura_janela // 2 )) 
+
+            pygame.draw.rect(tela, PRETO, (0, 0, tamanho_janela[0], tamanho_janela[1]))
+            tela.blit(texto_reinit, ret_reinit)
+
+            pygame.display.flip()
+            pygame.time.wait(1300)
+
+            obter_estado_inicial()
+
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Botão esquerdo do mouse pressionado
             if not selecionado:
                 # Verifica se uma peça foi selecionada
@@ -113,6 +129,7 @@ while executando:
 
         texto_titulo = fonte_titulo.render("Jogo de Damas", True, BRANCO)
         texto_novo = fonte_texto.render("NOVO JOGO (ENTER)", True, BRANCO)
+        texto_reiniciar = fonte_texto.render("REINICIAR PARTIDA (R)", True, BRANCO)
         texto_salvar = fonte_texto.render("SALVAR PARTIDA (IMPLEMENTAR)", True, BRANCO)
         texto_historico = fonte_texto.render("HISTÓRICO DE PARTIDAS (IMPLEMENTAR)", True, BRANCO)
         texto_sair = fonte_texto.render("SAIR (F2)", True, BRANCO)
@@ -120,12 +137,14 @@ while executando:
 
         ret_titulo = texto_titulo.get_rect(center=(largura_janela // 2, altura_janela // 2 - 125))
         ret_novo = texto_novo.get_rect(center=(largura_janela // 2, altura_janela // 2))
-        ret_salvar = texto_salvar.get_rect(center=(largura_janela // 2, altura_janela // 2 + 50))
-        ret_historico = texto_historico.get_rect(center=(largura_janela // 2, altura_janela // 2 + 100))
-        ret_sair = texto_sair.get_rect(center=(largura_janela // 2, altura_janela // 2 + 150))
+        ret_reiniciar = texto_reiniciar.get_rect(center=(largura_janela // 2, altura_janela // 2 + 50))
+        ret_salvar = texto_salvar.get_rect(center=(largura_janela // 2, altura_janela // 2 + 100))
+        ret_historico = texto_historico.get_rect(center=(largura_janela // 2, altura_janela // 2 + 150))
+        ret_sair = texto_sair.get_rect(center=(largura_janela // 2, altura_janela // 2 + 200))
 
         tela.blit(texto_titulo, ret_titulo)
         tela.blit(texto_novo, ret_novo)
+        tela.blit(texto_reiniciar, ret_reiniciar) 
         tela.blit(texto_salvar, ret_salvar)
         tela.blit(texto_historico, ret_historico)
         tela.blit(texto_sair, ret_sair)
@@ -171,20 +190,13 @@ while executando:
             y = (linha * tamanho_quadrado + 40)
             pygame.draw.circle(tela, PRETO, (x, y), tamanho_peca)
 
-        # Desenhar peça selecionada
-        if selecionado and pos_selecionada:
-            linha, coluna = pos_selecionada
-            x = (coluna * tamanho_quadrado + 40)
-            y = (linha * tamanho_quadrado + 40)
-            pygame.draw.circle(tela, AMARELO, (x, y), tamanho_peca)
-
         # Destacar casas disponíveis para movimentação
         if selecionado and pos_selecionada:
             casas_disponiveis = obter_casas_disponiveis(pos_selecionada[0], pos_selecionada[1])
             for linha, coluna in casas_disponiveis:
                 x = (coluna * tamanho_quadrado)
                 y = (linha * tamanho_quadrado)
-                pygame.draw.rect(tela, AZUL, (x, y, tamanho_quadrado, tamanho_quadrado))
+                pygame.draw.rect(tela, AMARELO, (x, y, tamanho_quadrado, tamanho_quadrado))
 
         if vez_das_brancas:
             fonte_vez = pygame.font.Font(None, 30)
