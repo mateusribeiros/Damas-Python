7ac8686de45f8d5ddcd2f0548e30a7db8c64e639
Mateus Eduardo <mateuseduardosilvar@gmail.com>
2023-06-10 21:53:33 -0300

    Add peças no tabuleiro

--- Damas/myDamas.py
+++ Damas/myDamas.py
@@ -3,7 +3,7 @@ import pygame
 # Definindo as cores
 PRETO = (0, 0, 0)
 BRANCO = (255, 255, 255)
-VERDE = (0, 255, 0)
+VERDE = (47,79,47)
 
 # Inicialização do Pygame
 pygame.init()
@@ -21,34 +21,51 @@ pygame.display.set_caption("Jogo de Damas")
 numeros_linhas = ['1', '2', '3', '4', '5', '6', '7', '8']
 letras_colunas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
-# Variável para controlar o estado do jogo
-no_menu = True
+# Posições Iniciais das Peças de damas
+pos_iniciais_brancas = [
+    (0,1), (0,3), (0,5), (0,7),
+    (1,0), (1,2), (1,4), (1,6),
+    (2,1), (2,3), (2,5), (2,7),
+]
 
-# Loop principal do jogo
+pos_iniciais_pretas = [
+    (5,0), (5,2), (5,4), (5,6),
+    (6,1), (6,3), (6,5), (6,7),
+    (7,0), (7,2), (7,4), (7,6),
+]
+
+# Loop principal do jogo e estado do jogo
+no_menu = True
 executando = True
 while executando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_F2):
             executando = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            no_menu = True
 
     if no_menu:
         # Lógica do menu
         tela.fill(PRETO)
+
         fonte_titulo = pygame.font.Font(None, 48)
         fonte_texto = pygame.font.Font(None, 26)
 
         texto_titulo = fonte_titulo.render("Jogo de Damas", True, BRANCO)
         texto_novo = fonte_texto.render("NOVO JOGO (ENTER)", True, BRANCO)
-        texto_historico = fonte_texto.render("RECUPERAR PARTIDA (IMPLEMENTAR)", True, BRANCO)
+        texto_salvar = fonte_texto.render("SALVAR PARTIDA (IMPLEMENTAR)", True, BRANCO)
+        texto_historico = fonte_texto.render("HISTÓRICO DE PARTIDAS (IMPLEMENTAR)", True, BRANCO)
         texto_sair = fonte_texto.render("SAIR (F2)", True, BRANCO)
 
-        ret_titulo = texto_titulo.get_rect(center=(largura_janela // 2, altura_janela // 2 - 100))
+        ret_titulo = texto_titulo.get_rect(center=(largura_janela // 2, altura_janela // 2 - 50))
         ret_novo = texto_novo.get_rect(center=(largura_janela // 2, altura_janela // 2))
-        ret_historico = texto_historico.get_rect(center=(largura_janela // 2, altura_janela // 2 + 50))
-        ret_sair = texto_sair.get_rect(center=(largura_janela // 2, altura_janela // 2 + 100))
+        ret_salvar = texto_salvar.get_rect(center=(largura_janela // 2, altura_janela // 2 + 50))
+        ret_historico = texto_historico.get_rect(center=(largura_janela // 2, altura_janela // 2 + 100))
+        ret_sair = texto_sair.get_rect(center=(largura_janela // 2, altura_janela // 2 + 150))
 
         tela.blit(texto_titulo, ret_titulo)
         tela.blit(texto_novo, ret_novo)
+        tela.blit(texto_salvar, ret_salvar)
         tela.blit(texto_historico, ret_historico)
         tela.blit(texto_sair, ret_sair)
 
@@ -70,13 +87,28 @@ while executando:
         for i, numero in enumerate(numeros_linhas):
             fonte = pygame.font.Font(None, 36)
             texto = fonte.render(numero, True, BRANCO)
-            tela.blit(texto, (largura_janela - 25, i * tamanho_quadrado + tamanho_quadrado // 2))
+            tela.blit(texto, (largura_janela - 25, i * tamanho_quadrado + 30))
 
         # Desenhar letras das colunas
         for i, letra in enumerate(letras_colunas):
             fonte = pygame.font.Font(None, 36)
             texto = fonte.render(letra, True, BRANCO)
-            tela.blit(texto, (i * tamanho_quadrado + tamanho_quadrado // 2, altura_janela - 25))
+            tela.blit(texto, (i * tamanho_quadrado + 30 , altura_janela - 25))
+
+        # Desenhaar peças BRANCAS
+        tamanho_peca = tamanho_quadrado // 3
+        for linha, coluna in pos_iniciais_brancas:
+            x = (coluna * tamanho_quadrado + 40)
+            y = (linha * tamanho_quadrado + 40)
+            pygame.draw.circle(tela, BRANCO, (x,y), tamanho_peca)
+        
+        # Desenhaar peças BRANCAS
+        for linha, coluna in pos_iniciais_pretas:
+            x = (coluna * tamanho_quadrado + 40)
+            y = (linha * tamanho_quadrado + 40)
+            pygame.draw.circle(tela, PRETO, (x,y), tamanho_peca)
+        
+        
 
     # Atualização da tela
     pygame.display.flip()
