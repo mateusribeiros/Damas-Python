1669d5851aa3f3197aeea62063475ab34af551aa
Mateus Eduardo <mateuseduardosilvar@gmail.com>
2023-06-10 22:53:39 -0300

    Movimentação das peças

--- Damas/myDamas.py
+++ Damas/myDamas.py
@@ -3,15 +3,22 @@ import pygame
 # Definindo as cores
 PRETO = (0, 0, 0)
 BRANCO = (255, 255, 255)
-VERDE = (47,79,47)
+VERDE = (47, 79, 47)
+AMARELO = (217, 217, 25)
+AZUL = (30, 144, 255)
+LARANJA = (255,165,0)
+AMARELO_ESVERDEADO = (153,204,50)
+ROSA = (255,0,255)
+AZUL_MARINHO = (112,147,219)
+VERMELHO = (255,0,0)
 
 # Inicialização do Pygame
 pygame.init()
 
 # Configuração da janela do jogo
 largura_janela = 650
-altura_janela = 650
-tamanho_quadrado = min(int(largura_janela * 0.95)  // 8, int(altura_janela  * 0.95)// 8)
+altura_janela = 700
+tamanho_quadrado = min(int(largura_janela * 0.95) // 8, int(altura_janela * 0.95) // 8)
 
 tamanho_janela = (largura_janela, altura_janela)
 tela = pygame.display.set_mode(tamanho_janela)
@@ -23,16 +30,32 @@ letras_colunas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
 # Posições Iniciais das Peças de damas
 pos_iniciais_brancas = [
-    (0,1), (0,3), (0,5), (0,7),
-    (1,0), (1,2), (1,4), (1,6),
-    (2,1), (2,3), (2,5), (2,7),
+    (0, 1), (0, 3), (0, 5), (0, 7),
+    (1, 0), (1, 2), (1, 4), (1, 6),
+    (2, 1), (2, 3), (2, 5), (2, 7),
 ]
 
 pos_iniciais_pretas = [
-    (5,0), (5,2), (5,4), (5,6),
-    (6,1), (6,3), (6,5), (6,7),
-    (7,0), (7,2), (7,4), (7,6),
+    (5, 0), (5, 2), (5, 4), (5, 6),
+    (6, 1), (6, 3), (6, 5), (6, 7),
+    (7, 0), (7, 2), (7, 4), (7, 6),
 ]
+
+# Variáveis de controle do jogo
+selecionado = False
+pos_selecionada = None
+vez_das_brancas = True
+
+# Função para obter as casas disponíveis para movimento
+def obter_casas_disponiveis(linha, coluna):
+    casas_disponiveis = []
+    if vez_das_brancas:
+        casas_disponiveis.append((linha + 1, coluna + 1))
+        casas_disponiveis.append((linha + 1, coluna - 1))
+    else:
+        casas_disponiveis.append((linha - 1, coluna + 1))
+        casas_disponiveis.append((linha - 1, coluna - 1))
+    return casas_disponiveis
 
 # Loop principal do jogo e estado do jogo
 no_menu = True
@@ -43,21 +66,59 @@ while executando:
             executando = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             no_menu = True
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Botão esquerdo do mouse pressionado
+            if not selecionado:
+                # Verifica se uma peça foi selecionada
+                pos_mouse = pygame.mouse.get_pos()
+                coluna = pos_mouse[0] // tamanho_quadrado
+                linha = pos_mouse[1] // tamanho_quadrado
+                if vez_das_brancas:
+                    if (linha, coluna) in pos_iniciais_brancas:
+                        selecionado = True
+                        pos_selecionada = (linha, coluna)
+                else:
+                    if (linha, coluna) in pos_iniciais_pretas:
+                        selecionado = True
+                        pos_selecionada = (linha, coluna)
+            else:
+                # Movimenta a peça selecionada para a casa diagonalmente adjacente
+                pos_mouse = pygame.mouse.get_pos()
+                coluna = pos_mouse[0] // tamanho_quadrado
+                linha = pos_mouse[1] // tamanho_quadrado
+                casas_disponiveis = obter_casas_disponiveis(pos_selecionada[0], pos_selecionada[1])
+                if (linha, coluna) in casas_disponiveis:
+                    # Verifica se a casa selecionada está vazia
+                    if vez_das_brancas:
+                        if (linha, coluna) not in pos_iniciais_brancas:
+                            # Atualiza a posição da peça para a nova casa
+                            pos_iniciais_brancas.remove(pos_selecionada)
+                            pos_iniciais_brancas.append((linha, coluna))
+                            vez_das_brancas = False
+                    else:
+                        if (linha, coluna) not in pos_iniciais_pretas:
+                            # Atualiza a posição da peça para a nova casa
+                            pos_iniciais_pretas.remove(pos_selecionada)
+                            pos_iniciais_pretas.append((linha, coluna))
+                            vez_das_brancas = True
+                selecionado = False
+                pos_selecionada = None
 
     if no_menu:
         # Lógica do menu
         tela.fill(PRETO)
 
-        fonte_titulo = pygame.font.Font(None, 48)
+        fonte_titulo = pygame.font.Font(None, 76)
         fonte_texto = pygame.font.Font(None, 26)
+        fonte_desc = pygame.font.Font(None, 16)
 
         texto_titulo = fonte_titulo.render("Jogo de Damas", True, BRANCO)
         texto_novo = fonte_texto.render("NOVO JOGO (ENTER)", True, BRANCO)
         texto_salvar = fonte_texto.render("SALVAR PARTIDA (IMPLEMENTAR)", True, BRANCO)
         texto_historico = fonte_texto.render("HISTÓRICO DE PARTIDAS (IMPLEMENTAR)", True, BRANCO)
         texto_sair = fonte_texto.render("SAIR (F2)", True, BRANCO)
+        texto_msg = fonte_desc.render("Durante a partida pressione ESC para retornar ao MENU", True, LARANJA)
 
-        ret_titulo = texto_titulo.get_rect(center=(largura_janela // 2, altura_janela // 2 - 50))
+        ret_titulo = texto_titulo.get_rect(center=(largura_janela // 2, altura_janela // 2 - 125))
         ret_novo = texto_novo.get_rect(center=(largura_janela // 2, altura_janela // 2))
         ret_salvar = texto_salvar.get_rect(center=(largura_janela // 2, altura_janela // 2 + 50))
         ret_historico = texto_historico.get_rect(center=(largura_janela // 2, altura_janela // 2 + 100))
@@ -68,11 +129,13 @@ while executando:
         tela.blit(texto_salvar, ret_salvar)
         tela.blit(texto_historico, ret_historico)
         tela.blit(texto_sair, ret_sair)
+        tela.blit(texto_msg, (10, 680))
 
         # Verifica se o jogador pressionou a tecla ENTER para iniciar o jogo
         teclas_pressionadas = pygame.key.get_pressed()
         if teclas_pressionadas[pygame.K_RETURN]:
             no_menu = False
+            vez_das_brancas = True
 
     else:
         # Lógica do jogo e desenho do tabuleiro
@@ -93,25 +156,46 @@ while executando:
         for i, letra in enumerate(letras_colunas):
             fonte = pygame.font.Font(None, 36)
             texto = fonte.render(letra, True, BRANCO)
-            tela.blit(texto, (i * tamanho_quadrado + 30 , altura_janela - 25))
+            tela.blit(texto, (i * tamanho_quadrado + 30, altura_janela - 80))
 
-        # Desenhaar peças BRANCAS
+        # Desenhar peças BRANCAS
         tamanho_peca = tamanho_quadrado // 3
         for linha, coluna in pos_iniciais_brancas:
             x = (coluna * tamanho_quadrado + 40)
             y = (linha * tamanho_quadrado + 40)
-            pygame.draw.circle(tela, BRANCO, (x,y), tamanho_peca)
-        
-        # Desenhaar peças BRANCAS
+            pygame.draw.circle(tela, BRANCO, (x, y), tamanho_peca)
+
+        # Desenhar peças PRETAS
         for linha, coluna in pos_iniciais_pretas:
             x = (coluna * tamanho_quadrado + 40)
             y = (linha * tamanho_quadrado + 40)
-            pygame.draw.circle(tela, PRETO, (x,y), tamanho_peca)
-        
-        
+            pygame.draw.circle(tela, PRETO, (x, y), tamanho_peca)
 
-    # Atualização da tela
+        # Desenhar peça selecionada
+        if selecionado and pos_selecionada:
+            linha, coluna = pos_selecionada
+            x = (coluna * tamanho_quadrado + 40)
+            y = (linha * tamanho_quadrado + 40)
+            pygame.draw.circle(tela, AMARELO, (x, y), tamanho_peca)
+
+        # Destacar casas disponíveis para movimentação
+        if selecionado and pos_selecionada:
+            casas_disponiveis = obter_casas_disponiveis(pos_selecionada[0], pos_selecionada[1])
+            for linha, coluna in casas_disponiveis:
+                x = (coluna * tamanho_quadrado)
+                y = (linha * tamanho_quadrado)
+                pygame.draw.rect(tela, AZUL, (x, y, tamanho_quadrado, tamanho_quadrado))
+
+        if vez_das_brancas:
+            fonte_vez = pygame.font.Font(None, 30)
+            texto_vez = fonte_vez.render("Vez das BRANCAS!", True, LARANJA)
+            tela.blit(texto_vez, (10, 10))
+        else:
+            fonte_vez = pygame.font.Font(None, 30)
+            texto_vez = fonte_vez.render("Vez das PRETAS!", True, LARANJA)
+            tela.blit(texto_vez, (10, 10))
+
     pygame.display.flip()
 
-# Encerramento do Pygame
 pygame.quit()
+
